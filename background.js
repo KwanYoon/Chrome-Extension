d584414d1159e847af879755ce0dcf478fe0e88c
KwanYoon <47330978+KwanYoon@users.noreply.github.com>
2020-12-22 22:26:28 -0500

    update button

--- background.js
+++ background.js
@@ -3,18 +3,6 @@ chrome.tabs.onActivated.addListener(tab => {
         if (/^https:\/\/store\.steampowered\.com\/app\//.test(current_tab_info.url)) {
             chrome.tabs.insertCSS(null, {file: "./mystyles.css"})
             chrome.tabs.executeScript(null, {file: "./foreground.js"})
-
-            url = current_tab_info.url;
-            slashLoc = 0;
-            for (i = url.length - 2; i > 0; i--) {
-                if (url.charAt(i) == '/') {
-                    slashLoc = i;
-                    i = 0;
-                }
-            }
-
-            gogUrl = "https://www.gog.com/game/" + url.substring(slashLoc + 1, url.length - 1);
-            console.log(gogUrl)
         }
     })
 });
--- popup.js
+++ popup.js
@@ -1,1 +1,22 @@
-document.getElementById("gog-url").innerHTML = "test";
+var gogUrl = "https://www.gog.com";
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var currTab = tabs[0];
+    if (currTab) {
+        url = currTab.url;
+        slashLoc = 0;
+        for (i = url.length - 2; i > 0; i--) {
+            if (url.charAt(i) == '/') {
+                slashLoc = i;
+                i = 0;
+            }
+        }
+
+        gogUrl = "https://www.gog.com/game/" + url.substring(slashLoc + 1, url.length - 1);
+        document.getElementById("gog-url").onclick = gogHyperlink(gogUrl);
+    }
+});
+
+function gogHyperlink(link) {
+    window.open(link)
+}
